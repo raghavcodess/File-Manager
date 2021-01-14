37fa28a7a15fb87d108d5833d8a6e18e285877fe
RAGHAV SHARMA <58854732+raghavcodess@users.noreply.github.com>
2021-01-14 15:13:55 +0530

    Add files via upload
    GUI Based File-Manager

--- sample.py
+++ sample.py
@@ -1,24 +1,42 @@
-#import modules
-import os,shutil
+from tkinter import *
+root=Tk()
 
-#change of directory
-path=input("Paste Directory:")
-os.chdir(path)
+root.title("File Manager")
+e=Entry(root)
+e.insert(0,"")
 
-#iterate through files
-for (path,dir,files) in os.walk(path):
-    for file in files:
-        #to get extension of files
-        ext=file.split(".")[1] 
-        #to check either directory exist
-        #if exist
-        if os.path.exists(path+"/"+ext):
-            if file.endswith(ext):
+
+def file_manager():
+    #import modules
+    import os,shutil
+
+    #change of directory    
+    path=e.get()
+    os.chdir(path)
+
+    #iterate through files
+    for (path,dir,files) in os.walk(path):
+        for file in files:
+            #to get extension of files
+            ext=file.split(".")[1] 
+            #to check either directory exist
+            #if exist
+            if os.path.exists(path+"/"+ext):
+                if file.endswith(ext):
+                    #move file to its respective directory
+                    shutil.move(file,path+"/"+ext)
+            #if doesn't exists
+            else:
+                #to create directory
+                os.system("mkdir "+ext)
                 #move file to its respective directory
                 shutil.move(file,path+"/"+ext)
-        #if doesn't exists
-        else:
-            #to create directory
-            os.system("mkdir "+ext)
-            #move file to its respective directory
-            shutil.move(file,path+"/"+ext)
+
+e.pack()
+myButton=Button(
+    root,
+    text="Submit",
+    command=file_manager)
+
+myButton.pack()
+root.mainloop()
